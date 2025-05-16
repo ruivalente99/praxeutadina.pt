5318524b13e93a7878a91d1025862ca94a06cfef
Rui  Valente <rui.valente99@gmail.com>
2025-05-16 10:41:29 +0100

    Update Archive

--- data/resources.ts
+++ data/resources.ts
@@ -1,10 +1,36 @@
-import { Scroll } from 'lucide-react';
+import { Clock, Scroll } from 'lucide-react';
 
 export const resources = [
   {
     name: "Os 10 Mandamentos de Baco",
+    isArchive: false,
     description: "Código da Praxe",
     icon: Scroll,
-    url: "/docs/codigopraxe.pdf"
+    url: "/docs/codigodepraxe.pdf",
+    date: "2023"
+  },
+  {
+    name: "Código de Praxe 2013",
+    isArchive: true,
+    description: "Código da Praxe",
+    icon: Clock,
+    url: "/docs/codigodepraxe2013.pdf",
+    date: "2013"
+  },
+  {
+    name: "Código de Praxe 2011",
+    isArchive: true,
+    description: "Código da Praxe",
+    icon: Clock,
+    url: "/docs/codigodepraxe2011.pdf",
+    date: "2011"
+  },
+  {
+    name: "Código de Praxe 2007",
+    isArchive: true,
+    description: "Código da Praxe",
+    icon: Clock,
+    url: "/docs/codigodepraxe2007.pdf",
+    date: "2007"
   },
 ];
